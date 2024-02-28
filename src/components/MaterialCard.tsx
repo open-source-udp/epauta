4356e5214cac1c6f6f696cee9b2d23bc98133bab
Samuel Angulo <samuel.angulo_c@mail.udp.cl>
2024-02-27 23:35:10 -0300

    start2

--- src/components/MaterialCard.tsx
+++ src/components/MaterialCard.tsx
@@ -1,34 +1,25 @@
-import { useEffect } from 'react'
-import { supabase } from "../lib/db"
-
 import {
-	Card,
-	CardHeader,
-} from "./ui/card"
+  Card,
+  CardHeader
+} from './ui/card'
 
 interface Props {
-	nombre: string
-	codigo: string
-	publicUrl: string
+  nombre: string
+  codigo: string
+  publicUrl: string
 }
 
-export default function MaterialCard({ nombre, codigo, publicUrl }: Props) {
-
-	const { data } = supabase
-		.storage
-		.from('recursos')
-		.getPublicUrl(`${codigo}/${nombre}`)
-
-	return (
+export default function MaterialCard ({ nombre, codigo, publicUrl }: Props) {
+  return (
 		<a href={publicUrl} ><Card style={{
-			backgroundColor: "#f3f3f3",
-			borderRadius: "0.5rem",
+		  backgroundColor: '#f3f3f3',
+		  borderRadius: '0.5rem'
 		}}>
 			<CardHeader style={{
-				fontSize: "15px",
-				textTransform: "uppercase",
-				fontWeight: "bold",
+			  fontSize: '15px',
+			  textTransform: 'uppercase',
+			  fontWeight: 'bold'
 			}}>{nombre}</CardHeader>
 		</Card></a>
-	)
+  )
 }
