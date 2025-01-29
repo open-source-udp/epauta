1d046a2ca0c75004ba68c1ed861e9cc516d6e1d8
polaarts <samuel.angulo_c@mail.udp.cl>
2025-01-29 02:09:57 -0300

    fix: improve MaterialCard styling for better responsiveness

--- src/components/MaterialCard.jsx
+++ src/components/MaterialCard.jsx
@@ -1,19 +1,25 @@
 const MaterialCard = ({ material, onClick }) => {
   return (
     <div
-      style={{
-        backgroundColor: "#f3f3f3",
-        borderRadius: "0.5rem",
-        padding: "1rem",
-        cursor: "pointer",
-      }}
+    style={{
+      backgroundColor: "#f3f3f3",
+      borderRadius: "0.5rem",
+      padding: "1rem",
+      cursor: "pointer",
+      height: "100%",
+      width: '100%',  
+    }}
       onClick={() => onClick(material.publicUrl)}
     >
       <h3
-        style={{
+         style={{
           fontSize: "15px",
           textTransform: "uppercase",
           margin: 0,
+          whiteSpace: "nowrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis", 
+          maxWidth: "100%",  
         }}
       >
         {material.nombre}
